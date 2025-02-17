6c6348ea79f4aee7215766103e91054c477fdb79
AdenilsonSantos <62746631+AdenilsonSantos@users.noreply.github.com>
2025-02-17 14:53:59 -0300

    Install Visual C++ using Intune.
    Utilize esse repositório para instalar a versão 14.42.34438.0 utilizando Win32App via Intune

---
+++ VisualC++Redist/Install-file.ps1
@@ -0,0 +1,24 @@
+# Script para desinstalar todas as versões do 7-Zip e instalar a versão mais recente
+
+# Variável para o caminho do instalador do 7-Zip
+$zipInstallerPath = "VC_redist_x86.exe"
+$ScriptPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
+
+#Função para desinstalar o VC_redist_x86 EXE
+function Uninstall-VC_redist_x86-Exe {
+    $product = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE '%Microsoft Visual C++%' AND Name LIKE '%x86%'"
+   $productName = $product.Name
+   Uninstall-Package -Name $productName -Force
+}
+
+# Função para instalar a versão mais recente do VC_redist_x86
+function Install-VC_redist_x86-Exe {
+    Start-Process -FilePath "$ScriptPath\$zipInstallerPath" -ArgumentList "/install", "/quiet", "/norestart" -Wait
+}
+
+# Desinstalar o VC_redist_x86 EXE
+Uninstall-VC_redist_x86-Exe
+
+
+# Instalar a versão mais recente do VC_redist_x86 Exe
+Install-VC_redist_x86-Exe
